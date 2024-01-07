0317b0744c53b45db751cc567a76340faf5b2b46
HalfToothed <abhishekandmails@gmail.com>
2024-01-07 10:57:00 +0530

    json

---
+++ Data.cs
@@ -0,0 +1,73 @@
+
+namespace Data;
+
+ public class AtlasData
+    {
+        public string country { get; set; }
+        public string country_long { get; set; }
+        public string currency { get; set; }
+        public string capital_city { get; set; }
+        public string region { get; set; }
+        public string continent { get; set; }
+        public string demonym { get; set; }
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+        public double agricultural_land { get; set; }
+        public double forest_area { get; set; }
+        public double land_area { get; set; }
+        public double rural_land { get; set; }
+        public double urban_land { get; set; }
+        public double central_government_debt_pct_gdp { get; set; }
+        public double expense_pct_gdp { get; set; }
+        public string gdp { get; set; }
+        public double inflation { get; set; }
+        public double self_employed_pct { get; set; }
+        public double tax_revenue_pct_gdp { get; set; }
+        public double unemployment_pct { get; set; }
+        public double vulnerable_employment_pct { get; set; }
+        public double electricity_access_pct { get; set; }
+        public double alternative_nuclear_energy_pct { get; set; }
+        public double electricty_production_coal_pct { get; set; }
+        public double electricty_production_hydroelectric_pct { get; set; }
+        public double electricty_production_gas_pct { get; set; }
+        public double electricty_production_nuclear_pct { get; set; }
+        public double electricty_production_oil_pct { get; set; }
+        public double electricty_production_renewable_pct { get; set; }
+        public double energy_imports_pct { get; set; }
+        public double fossil_energy_consumption_pct { get; set; }
+        public double renewable_energy_consumption_pct { get; set; }
+        public double co2_emissions { get; set; }
+        public double methane_emissions { get; set; }
+        public double nitrous_oxide_emissions { get; set; }
+        public double greenhouse_other_emissions { get; set; }
+        public double urban_population_under_5m { get; set; }
+        public double health_expenditure_pct_gdp { get; set; }
+        public double health_expenditure_capita { get; set; }
+        public double hospital_beds { get; set; }
+        public double hiv_incidence { get; set; }
+        public double suicide_rate { get; set; }
+        public double armed_forces { get; set; }
+        public double doubleernally_displaced_persons { get; set; }
+        public double military_expenditure_pct_gdp { get; set; }
+        public double birth_rate { get; set; }
+        public double death_rate { get; set; }
+        public double fertility_rate { get; set; }
+        public double doubleernet_pct { get; set; }
+        public double life_expectancy { get; set; }
+        public double net_migration { get; set; }
+        public double population_female { get; set; }
+        public double population_male { get; set; }
+        public double population { get; set; }
+        public double women_parliament_seats_pct { get; set; }
+        public double rural_population { get; set; }
+        public double urban_population { get; set; }
+        public double press { get; set; }
+        public double democracy_score { get; set; }
+        public string democracy_type { get; set; }
+        public double median_age { get; set; }
+        public string political_leader { get; set; }
+        public string title { get; set; }
+    }
+
+
+
--- Program.cs
+++ Program.cs
@@ -1,4 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
+using System.Text.Json;
+using Data;
+using System.Linq;
 
+namespace Program;
 
+string filePath = "atlas.json";
+string jsonData = File.ReadAllText(filePath);
+
+List<AtlasData> myAtlasObject = JsonSerializer.Deserialize<List<AtlasData>>(jsonData);
+
+var data = myAtlasObject.Where(x => x.latitude > 33);
+
+foreach (var item in data)
+{
+    Console.WriteLine($"Country: {item.country}, Latitude: {item.latitude}");
+}
